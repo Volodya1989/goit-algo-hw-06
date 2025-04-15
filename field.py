a3dd761a5a8de68f1aaf9d8eda17385c3995f78d
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2025-04-15 18:28:51 -0400

    separated classes into different files

---
+++ field.py
@@ -0,0 +1,11 @@
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    # реалізація класу
+    pass
--- record.py
+++ record.py
@@ -1,24 +1,5 @@
-from collections import UserDict
-
-
-class Field:
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
-
-
-class Name(Field):
-    # реалізація класу
-    pass
-
-
-class Phone(Field):
-    def __init__(self, value):
-        if not value.isdigit() or len(value) != 10:
-            raise ValueError("Phone number must have 10 digits.")
-        super().__init__(value)
+from phone import Phone
+from field import Name
 
 
 class Record:
@@ -53,17 +34,3 @@ class Record:
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-
-
-class AddressBook(UserDict):
-    def add_record(self, record):
-        self.data[record.name.value] = record
-
-    def find(self, name):
-        return self.data.get(name)
-
-    def delete(self, name):
-        self.data.pop(name, None)
-
-    def __str__(self):
-        return "\n".join(map(str, self.data.values()))
