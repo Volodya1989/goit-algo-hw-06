aabe1cda032f61c04163196d3fa2a8d05c543359
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2025-04-16 15:23:16 -0400

    fixed based on the feedback

--- record.py
+++ record.py
@@ -11,26 +11,23 @@ class Record:
         self.phones.append(Phone(phone_number))
 
     def remove_phone(self, phone_number):
-        phone_to_remove = next(
-            (p for p in self.phones if p.value == phone_number), None)
+        phone_to_remove = self.find_phone(phone_number)
         if phone_to_remove:
             self.phones.remove(phone_to_remove)
         else:
-            raise ValueError("Phone number is not found!")
+            raise ValueError("Phone number not found!")
 
-    def edit_phone(self, new_phone_number, old_phone_number):
-        phone_to_edit = next((i for i, p in enumerate(
-            self.phones) if p.value == old_phone_number), None)
-        if phone_to_edit is not None:
-            self.phones[phone_to_edit] = Phone(new_phone_number)
-            print(
-                f"Updated phone from {old_phone_number} to {new_phone_number}")
-
+    def edit_phone(self, old_phone_number, new_phone_number):
+        phone_to_edit = self.find_phone(old_phone_number)
+        if phone_to_edit:
+            self.remove_phone(old_phone_number)
+            self.add_phone(new_phone_number)
         else:
             raise ValueError(f"Phone number {old_phone_number} not found.")
 
     def find_phone(self, phone):
-        return next((p for p in self.phones if phone == p.value), None)
+        return next((p for p in self.phones if p.value == phone), None)
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        phones_str = "; ".join(p.value for p in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones_str}"
